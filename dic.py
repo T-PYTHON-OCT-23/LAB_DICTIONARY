fb4f5a8654d0df5207f0b975c5b1c855f6f36a3b
Hadeel-7i <147502790+Hadeel-7i@users.noreply.github.com>
2023-10-16 19:24:12 +0300

    Update dic.py

--- dic.py
+++ dic.py
@@ -24,3 +24,14 @@ if phone_input in contacts:
     print(f"The number for {phone_input} is {contacts[phone_input]}")
 else:
     print(f"Sorry we don't have the info for {phone_input}")
+  #2
+def rearrange(input_numbers):
+    numbers = [x for x in input_numbers if x != 0]
+    zero = [x for x in input_numbers if x == 0]
+    arranged_list = numbers + zero
+    return arranged_list
+
+input_numbers = [5, 0, 34, 9, 0, 13, 8]
+result = rearrange(input_numbers)
+print(result)
+
