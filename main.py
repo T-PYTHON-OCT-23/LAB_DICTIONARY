b6d0da315a8a881d33d63679a5f844628b50e124
AlbandariAljabri <Albandarialjabri@gmail.com>
2023-10-17 10:39:49 +0300

    dic lab solved

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ phone_book = {
  }
 
 user_input = input("Please enter the number: ")
+''' 
+# another soluation
+if len(user_input)==10 and user_input.isdigit() and user_input in phone_book:
+      owner= print(f"the {user_input} is {phone_book[user_input]}")
+else:
+    print("Sorry, the number is not found")
 
+    الفرق بين 
+    isdigit :
+تدعم الارقام فقط سواء الصحيحة ام الكسور
+    isnumeric :
+    شاملة اكثر
+'''    
 if user_input in phone_book:
     owner= print(phone_book[user_input])
 elif len(user_input) < 10 or len(user_input) > 10 or user_input.isnumeric():
@@ -18,14 +30,21 @@ elif len(user_input) < 10 or len(user_input) > 10 or user_input.isnumeric():
 else:
     print("Sorry, the number is not found")
 print()
+print(" ---------Q2-------------")
+print()
 
 #Q2
-new_list = ()
+
 def arrange (list_num:list):  
+    zerros_list = []
+
     for i in list_num:
-        if i==0  in list_num:
-            list_num.insert(-1,i)
-            return list_num
+        if i==0 :
+            zerros_list.append(i)
+        else:
+            zerros_list.insert(0,i)
+
+    return zerros_list
 
 
 list= [5, 0, 34, 9, 0, 13, 8]
