a6c4319ef025af73a52102ac7e57099eb0871553
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-10-19 14:28:04 +0300

    done

--- dictionary.py
+++ dictionary.py
@@ -7,22 +7,34 @@ my_d={"0568323222":"maram",
       "0567917077":"lamira"
       }
 
-input_user= input("enter number:")
-if input_user in my_d:
+input_user=input("enter number:" )
+if input_user in my_d :
+    if input_user.isnumeric()!= True:
+      print("This is invalid number")
+
     print (f"This is invalid number:{input_user} is {my_d[input_user]}")
 else:
     print("Sorry, the number is not found")
 
 
-l:[5, 0, 34, 9, 0, 13, 8]
-def lis(l):
-    for n in l:
-        if n ==0:
-          l.sort(l)
-       
-          return l
-print(lis())
+print("--"*40)
 
 
+
+def l_r(list1 )-> list:
+    list1.sort()
+    for n in list1:
+        if n in list1:
+            if n == 0:
+             list1.remove(n)
+             list1.append(n)
+
+    return list1
+
+
+list =  [5, 0, 34, 9, 0, 13, 8, -5]
+print(f" befor : {list}")
+m = l_r(list)
+print(f" after : {m}")
 
 
