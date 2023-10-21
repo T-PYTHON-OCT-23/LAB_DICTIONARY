57eb53088c89c72fc55891f51e86d30b5e74a802
hadeel.i7 <hadeel.naji175@gmail.com>
2023-10-21 18:20:02 +0300

    ubdate.dic

--- dic.py
+++ dic.py
@@ -10,17 +10,17 @@ for key in my_contacts:
     print(my_contacts)
 
 contacts = {
-    "Amal": "0568323222",
-    "Mohammed": "0522222232",
-    "Khadijah": "0532335983",
-    "Abdullah": "0545341144",
-    "Rawan": "0545534556",
-    "Faisal": "0560664566",
-    "Layla": "0567917077"
+    "0568323222" : "Amal",
+    "0522222232" : "Mohammed",
+    "0532335983" : "Khadijah",
+    "0545341144" : "Abdullah",
+    "0545534556" : "Rawan",
+    "0560664566" : "Faisal",
+    "0567917077" : "Layla"
 }
 
-phone_input = input("Enter Name: ")
+phone_input = input("Enter phonr number: ")
 if phone_input in contacts:
-    print(f"The number for {phone_input} is {contacts[phone_input]}")
+    print(f"The phone number {phone_input} is for {contacts[phone_input]}")
 else:
     print(f"Sorry we don't have the info for {phone_input}")
